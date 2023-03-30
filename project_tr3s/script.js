1fee77eac056b9106c0068d45c07df243276f9ed
Siebrex <98038100+Siebrex@users.noreply.github.com>
2023-03-30 10:15:25 -0300

    commit
    commit to main

--- project_tr3s/script.js
+++ project_tr3s/script.js
@@ -1,6 +1,8 @@
 function meuEscopo(){
     const form = document.querySelector('.forma');
+    const forms = document.querySelector('.formaA');
     const resultado =document.querySelector('.resultado');
+    const resultados =document.querySelector('.resultados');
 
     const pessoas = [];
 
@@ -10,17 +12,29 @@ function meuEscopo(){
         const celular = form.querySelector('.celular');
         const email = form.querySelector('.email');
 
+        const nome_containere = forms.querySelector('.nome_container');
+        const celulare = forms.querySelector('.celular');
+        const emaile = forms.querySelector('.email');
+
+
 
         pessoas.push({
             nome_container: nome_container.value,
             celular: celular.value,
-            email: email.value
+            email: email.value,
+            nome_container: nome_containere.value,
+            celular: celulare.value,
+            email: emaile.value
         });
         console.log(pessoas);
-        resultado.innerHTML += `<br></br><p> ${nome_container.value} seus dados foram enviados.</p>`
+        resultado.innerHTML += `<br></br><p> ${nome_container.value} seus dados foram enviados.</p>`;
+
+        console.log(pessoas);
+        resultados.innerHTML += `<br></br><p> ${nome_container.value} seus dados foram enviados.</p>`
     }
     
     form.addEventListener('submit', recebeEventoform);
+    forms.addEventListener('submit', recebeEventoform);
 
 }
 meuEscopo();
